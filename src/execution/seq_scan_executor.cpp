172d55387d2e4f1a31c6a47993ed8c3396b63c23
Chengqi Deng <checkdeng0903@gmail.com>
2021-05-15 17:27:29 +0800

    Implement seq scan

--- src/execution/seq_scan_executor.cpp
+++ src/execution/seq_scan_executor.cpp
@@ -13,10 +13,36 @@
 
 namespace bustub {
 
-SeqScanExecutor::SeqScanExecutor(ExecutorContext *exec_ctx, const SeqScanPlanNode *plan) : AbstractExecutor(exec_ctx) {}
+SeqScanExecutor::SeqScanExecutor(ExecutorContext *exec_ctx, const SeqScanPlanNode *plan) : AbstractExecutor(exec_ctx) {
+  auto catalog = exec_ctx->GetCatalog();
+  auto table_oid = plan->GetTableOid();
+  auto table = catalog->GetTable(table_oid);
 
-void SeqScanExecutor::Init() {}
+  table_heap_ = table->table_.get();
+  schema_ = &table->schema_;
+  predicate_ = plan->GetPredicate();
+}
 
-bool SeqScanExecutor::Next(Tuple *tuple, RID *rid) { return false; }
+void SeqScanExecutor::Init() {
+  auto it = table_heap_->Begin(exec_ctx_->GetTransaction());
+  iterator_= std::make_unique<TableIterator>(it);
+}
+
+bool SeqScanExecutor::Next(Tuple *tuple, RID *rid) {
+  auto &it = *iterator_;
+
+  while (it != table_heap_->End()) {
+    bool passed = predicate_->Evaluate(&(*it), schema_).GetAs<bool>();
+    if (passed) {
+      *tuple = *it;
+      *rid = tuple->GetRid();
+      it++;
+      return true;
+    }
+    it++;
+  }
+
+  return false;
+}
 
 }  // namespace bustub
